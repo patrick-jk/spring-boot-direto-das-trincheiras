e76e2d301cc1d5e7dc07814478b56b676981e0e3
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-08-11 12:26:30 -0300

    video09: Exercise 01 - GetMapping

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -2,14 +2,11 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"outside.devdojo", "academy.devdojo"})
 public class AnimeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeServiceApplication.class, args);
+    }
 }
