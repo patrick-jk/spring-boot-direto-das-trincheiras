5df2a779d93d7a4d7d417c6a5df59ce965ce843f
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-08-27 17:19:31 -0300

    video26: MVC Pattern - Repository

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -18,14 +16,4 @@ public class Producer {
     private Long id;
     private String name;
     private LocalDateTime createdAt;
-    @Getter
-    private static List<Producer> producers = new ArrayList<>();
-
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimation = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
-
-        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
-    }
 }
