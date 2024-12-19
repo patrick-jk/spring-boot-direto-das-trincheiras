b4615a40e1a2af32c92ae074a985bdde94e88943
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-12-19 18:47:03 -0300

    video74: adding spring boot starter data jpa dependency

--- user-service/src/main/java/academy/devdojo/request/UserPutRequest.java
+++ user-service/src/main/java/academy/devdojo/request/UserPutRequest.java
@@ -14,6 +14,7 @@ public class UserPutRequest {
     @NotNull(message = "The field 'id' cannot be null")
     private Long id;
     @NotBlank(message = "The field 'firstName' is required")
+    private String firstName;
     @NotBlank(message = "The field 'lastName' is required")
     private String lastName;
     @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "The e-mail is not valid")
