1f92ed73c60d86cc28256743c20e748d797537f5
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-12-25 17:32:20 -0300

    video89: Pagination

--- anime-service/src/main/java/academy/devdojo/anime/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/anime/AnimeController.java
@@ -3,6 +3,8 @@ package academy.devdojo.anime;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +20,7 @@ public class AnimeController {
     private final AnimeService service;
 
     @GetMapping
-    public ResponseEntity<List<AnimeGetResponse>> listAll(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<AnimeGetResponse>> findAll(@RequestParam(required = false) String name) {
         log.debug("Request received to list all animes, param name '{}'", name);
 
         var animes = service.findAll(name);
@@ -26,6 +28,15 @@ public class AnimeController {
         var animeGetResponses = mapper.toAnimeGetResponseList(animes);
 
         return ResponseEntity.ok(animeGetResponses);
+    }
+
+    @GetMapping("/paginated")
+    public ResponseEntity<Page<AnimeGetResponse>> findAll(Pageable pageable) {
+        log.debug("Request received to list all animes paginated");
+
+        var pageAnimeGetResponse = service.findAllPaginated(pageable).map(mapper::toAnimeGetResponse);
+
+        return ResponseEntity.ok(pageAnimeGetResponse);
     }
 
     @GetMapping("{id}")
