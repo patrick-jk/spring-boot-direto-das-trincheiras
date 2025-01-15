11a0419a3404c551bf5815f56ce90dfd03d7ae12
patrick-jk <patrick.pviolin@senacsp.edu.br>
2025-01-15 18:43:00 -0300

    video150: actuator and prometheus 01

--- anime-service/src/main/java/academy/devdojo/config/SecurityConfig.java
+++ anime-service/src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package academy.devdojo.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -35,6 +36,7 @@ public class SecurityConfig {
                         .requestMatchers(WHITE_LIST).permitAll()
                         .requestMatchers("v1/animes/**").hasRole("USER")
                         .requestMatchers("v1/producers/**").hasRole("USER")
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
